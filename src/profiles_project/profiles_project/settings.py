bdf263551bf45779217687f3c402d9bb0ced5122
Sam Russell <samrussell63@gmail.com>
2018-12-27 20:03:45 -0800

    update user model auth in django

--- src/profiles_project/profiles_project/settings.py
+++ src/profiles_project/profiles_project/settings.py
@@ -121,3 +121,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+
+# Override auth user model
+AUTH_USER_MODEL = 'profiles_api.UserProfile'
