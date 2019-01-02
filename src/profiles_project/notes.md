3e9a2aa9e47bb0842922c6b7f483f96311a34191
Sam Russell <samrussell63@gmail.com>
2019-01-01 20:52:10 -0800

    creating simple viewset

--- src/profiles_project/notes.md
+++ src/profiles_project/notes.md
@@ -12,5 +12,13 @@ Basic type of view - allows us to define functions for http methods
 * Full contro lover logic
 * Processing files and rendering a synchronous response
 
+### ViewSets
+Maps functions to operations - List, Create, Retrieve, Update, Partial Update, Destory
+* Perfect for standard database operations
+* Fastest way to make a database interface
+* Good for simple CRUD interface for databases
+    * when you don't need custom logic
+    * have standard data structures
+
 ### Serializers
 Converts json to python object
--- src/profiles_project/profiles_api/views.py
+++ src/profiles_project/profiles_api/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
+
+from rest_framework import viewsets
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
@@ -50,3 +52,15 @@ class HelloApiView(APIView):
     def patch(self, request, pk=None):
         """Patch request, only updates fields provided in the request."""
         return Response({'method': 'patch'})
+
+class HelloViewSet(viewsets.ViewSet):
+    """Test API ViewSet."""
+
+    def list(self, request):
+        """Return a hello message."""
+        a_viewset = [
+            'Uses actions (list, create, retrieve, update, partial_update)',
+            'Automatically maps to URLs using Routers',
+            'Provides more functionality with less code'
+        ]
+        return Response({'message': 'Hello!', 'a_viewset': a_viewset})
