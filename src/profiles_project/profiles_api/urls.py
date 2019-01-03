8e6434957d14e2cc2be675295855d405b8546f33
Sam Russell <samrussell63@gmail.com>
2019-01-02 20:31:49 -0800

    creating login api

--- src/profiles_project/profiles_api/urls.py
+++ src/profiles_project/profiles_api/urls.py
@@ -6,6 +6,7 @@ from . import views
 router = DefaultRouter()
 router.register('hello-viewset', views.HelloViewSet, basename='hello-viewset')
 router.register('profile', views.UserProfileViewSet) # uses the model as the basename
+router.register('login', views.LoginViewSet, basename='login')
 
 urlpatterns = [
     path('hello-view/', views.HelloApiView.as_view()),
